4e3c5b93533ccd1a23de40b59e74dcf99551ac0f
ashna23 <ashnaraikwar9@gmail.com>
2025-07-17 14:25:10 +0530

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-
+import { useState } from 'react'
 import './App.css'
-import Chai from './pages/chai'
+import Chai from './pages/counter'
 function App() {
   
+let [counter,setCounter] = useState(15)
 
+
+
+// let counter =15
+const addValue = () => {
+//  console.log("clicked",counter);
+ counter = counter + 1
+ setCounter(counter)
+
+}
+
+
+let removeValue = () => {
+  setCounter(counter -1)
+}
   return (
  
  < >
- < Chai />
-priyal
+    <h1>chai aur react</h1>
+    <h1>Counter value </h1>
+    <h1>{counter}</h1>
+    <button  onClick={addValue}>Add value{counter}</button>
+<br></br><br></br>
+
+     <button onClick={removeValue}>remove value</button>
  </> )
 }
 
